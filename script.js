d87c9992954e6c488839f2716aabd8d4cbd7585e
Madmouj7 <zohermadmooj2002@gmail.com>
2024-07-23 22:49:28 +0300

    some edits

--- script.js
+++ script.js
@@ -4,3 +4,46 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+var words = ['Software Developer', 'ADEL Platform Owner', 'Computer Science Graduate', 'Full Stack Developer'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+
+var wordflick = function() {
+  setInterval(function() {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      } else {
+        offset++;
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        if (i >= len) {
+          i = 0;
+        }
+      } else {
+        offset--;
+      }
+    }
+    part = words[i].substr(0, offset);
+    $('.word').text(part);
+  }, speed);
+};
+
+$(document).ready(function() {
+  wordflick();
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in-up': 'fadeInUp 1s ease-out',
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateY(20px)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateY(0)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
